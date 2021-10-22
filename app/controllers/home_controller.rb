0f4f510b2cbf57a67892092d959f86557fdb5ecd
Fordalex <alex96ford19@gmail.com>
2021-10-22 21:21:09 +0100

    removes js and uses partials to create time sheet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
     def home
+      @week_tickets = {
+        "#{Ticket.first.date.strftime("%A")}": Ticket.all.to_a,
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+        "Saturday": [],
+        "Sunday": []
+      }
     end
 end
--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -8,27 +8,7 @@ function openCalendar() {
   this.classList.add('calendar-open')
 }
 
-function styleTimeSheet(s) {
-  for (let i = 0; i < 24; i++) {
-    time = i < 12 ? " am" : " pm"
-    div = document.createElement('div');
-    div.classList.add('hour-container');
-    hr = document.createElement('hr');
-    div.appendChild(hr);
-    hourSpan = document.createElement('span');
-    hour = document.createTextNode(i + time);
-    hourSpan.appendChild(hour)
-    div.appendChild(hourSpan);
-    s.appendChild(div);
-  }
-}
-
 window.onload = function() {
   calendarContainers = document.querySelectorAll('.calendar-dayContainer')
   calendarContainers.forEach((c) => c.addEventListener('click', openCalendar))
-
-  timeSheets = document.querySelectorAll('.calendar-timeSheet');
-  timeSheets.forEach((s) => {
-    styleTimeSheet(s);
-  })
 }
