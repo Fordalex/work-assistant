06c9bcb74917d8d3b4559f64616268f55aba3e99
Fordalex <alex96ford19@gmail.com>
2021-10-22 14:57:01 +0100

    adds helper to create dates a week part

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def date_input(count)
+    list = []
+    count.times do
+      plus = (7 * list.length)
+      date = Date.new(2021, 10, 18) + plus.days
+      list << date.strftime("%d %B %Y")
+    end
+    list
+  end
 end
