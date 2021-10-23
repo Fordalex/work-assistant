273748aa09add8327f724fb3d342d77734bc8db7
Fordalex <alex96ford19@gmail.com>
2021-10-23 16:52:58 +0100

    adds category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_222142) do
+ActiveRecord::Schema.define(version: 2021_10_23_153218) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "colour"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string "title"
@@ -21,6 +28,8 @@ ActiveRecord::Schema.define(version: 2021_10_22_222142) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_tickets_on_category_id"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 RSpec.describe Ticket, type: :model do
   describe "Association" do
     it { should belong_to(:user) }
+    it { should belong_to(:category) }
   end
 end
