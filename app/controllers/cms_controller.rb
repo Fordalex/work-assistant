2cdd3a154d77a283e7fda10029fae616f0262268
Fordalex <alex96ford19@gmail.com>
2021-10-23 13:12:45 +0100

    adds cms link to navigation

---
+++ app/controllers/cms_controller.rb
@@ -0,0 +1,5 @@
+class CmsController < SessionsController
+  def cms
+    @categories = []
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "/home", to: "home#home", as: :home_path
   get "/notes", to: "notes#notes"
   get "/websites", to: "websites#websites"
+  get "/cms", to: "cms#cms"
+  post "/tickets", to: "tickets#create"
 
   devise_for :users
 end
