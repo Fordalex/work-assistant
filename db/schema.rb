f894eab85bb5c3f78bfffaf7b0f0b8fbfeaeb6ba
Fordalex <alex96ford19@gmail.com>
2021-10-23 11:13:16 +0100

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 2021_10_22_222142) do
     t.date "date"
     t.time "start_time"
     t.integer "duration"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
