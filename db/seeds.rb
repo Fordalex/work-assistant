0b25870145cc2c6f852b9c6e423e840aa6cb65d0
Fordalex <alex96ford19@gmail.com>
2021-10-23 16:53:16 +0100

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
 Ticket.destroy_all
+User.destroy_all
+
+puts "\n== Creating users =="
 
 user = User.create!(
   email: "test@example.com",
   password: "password"
 )
 
+puts "\n== Creating categories =="
+
+mhp = Category.create(
+  name: "MHP",
+  colour: "#65e33b"
+)
+
+mcf = Category.create(
+  name: "MCF",
+  colour: "#6cc9f5"
+)
+
+puts "\n== Creating tickets =="
+
 Ticket.create(
   user: user,
+  category: mcf,
   date: Date.new(2021, 10, 18),
   title: "MHP",
   description: "Made some change to the home page.",
@@ -23,6 +42,7 @@ Ticket.create(
 
 Ticket.create(
   user: user,
+  category: mcf,
   date: Date.new(2021, 10, 18),
   title: "MHP",
   description: "This is anther ticket I've created for some test data so it doesn't look poop...",
@@ -31,6 +51,7 @@ Ticket.create(
 )
 Ticket.create(
   user: user,
+  category: mcf,
   date: Date.new(2021, 10, 18),
   title: "MHP",
   description: "Fix layout issue with the parks navigation, Tony has request this to be changed.",
@@ -39,6 +60,7 @@ Ticket.create(
 )
 Ticket.create(
   user: user,
+  category: mcf,
   date: Date.new(2021, 10, 18),
   title: "MHP",
   description: "Had to pair as I needed help on a ticket, the issue was much harder that I first thought.",
