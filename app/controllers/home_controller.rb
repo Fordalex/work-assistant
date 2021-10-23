84bfcbeb3b84f328a6011e7fbac902c8161e7707
Fordalex <alex96ford19@gmail.com>
2021-10-23 11:51:50 +0100

    adds form and logic for search by start_date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,23 @@
 class HomeController < SessionsController
     def home
-      @week_tickets = {
-        "#{Ticket.first.date.strftime("%A")}": Ticket.all.to_a,
-        "Tuesday": [],
-        "Wednesday": [],
-        "Thursday": [],
-        "Friday": [],
-        "Saturday": [],
-        "Sunday": []
-      }
+      @start_date = date_params ? Date.parse(date_params) : Date.today
+
+      @week_tickets = {}
+
+      7.times do |index|
+        search_date = @start_date + index.days
+        day_tickets = Ticket.where(user: current_user, date: search_date)
+        if day_tickets.any?
+          @week_tickets["#{day_tickets.first&.date&.strftime("%A")}"] = day_tickets
+        else
+          @week_tickets["#{search_date.strftime("%A")}"] = []
+        end
+      end
+    end
+
+    private
+
+    def date_params
+      params[:start_date]
     end
 end
