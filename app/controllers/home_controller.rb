e0bf88433507eddf58da8f0af4d9f8561a524424
Fordalex <alex96ford19@gmail.com>
2021-10-23 13:11:41 +0100

    adds start date to session

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,27 @@
 class HomeController < SessionsController
     def home
-      @start_date = date_params ? Date.parse(date_params) : Date.today
+      @start_date = start_date
+      session[:start_date] = @start_date
 
       @week_tickets = {}
-
       7.times do |index|
         search_date = @start_date + index.days
         day_tickets = Ticket.where(user: current_user, date: search_date)
         @week_tickets["#{search_date.strftime("%A")}"] = day_tickets
       end
+
+      @ticket = Ticket.new
     end
 
     private
 
-    def date_params
-      params[:start_date]
+    def start_date
+      if params[:start_date]
+        Date.parse(params[:start_date])
+      elsif session[:start_date].present?
+        Date.parse(session[:start_date])
+      else
+        Date.today
+      end
     end
 end
