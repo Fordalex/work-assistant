a45056177c439546c14a2788194f04307e0ea5cf
Fordalex <alex96ford19@gmail.com>
2021-10-24 12:41:42 +0100

    removes unneeded code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,11 @@ class HomeController < SessionsController
       @start_date = start_date
       session[:start_date] = @start_date
 
-      @total_tickets = 0
-
       @week_tickets = {}
       7.times do |index|
         search_date = @start_date + index.days
         day_tickets = Ticket.where(user: current_user, date: search_date)
         @week_tickets["#{search_date.strftime("%A")}"] = {date: search_date, tickets: day_tickets}
-        @total_tickets += day_tickets.count
       end
 
       @ticket = Ticket.new
