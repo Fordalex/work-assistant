73beb1f72080c606a24cfdcc0353a059940ea9bd
Fordalex <alex96ford19@gmail.com>
2021-10-24 12:23:20 +0100

    calculate duration for tickets and display on home page

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -11,12 +11,25 @@ module TicketsHelper
     all_week_tickets(week_tickets).count
   end
 
-  def all_categories_with_count(week_tickets)
+  def all_categories_stats(week_tickets)
     dict = {}
     all_week_tickets(week_tickets).each do |ticket|
       category = ticket.category.name.to_s
-      dict[category] = dict.key?(category) ? (dict[category] + 1) : 1
+
+      if dict.key?(category)
+        dict[category][:count] += 1
+        dict[category][:duration] += ticket.duration
+      else
+        dict[category] = { count: 1, duration: ticket.duration }
+      end
+
     end
     dict.symbolize_keys
+  end
+
+  def time_conversion(minutes)
+    hours = minutes / 60
+    rest = minutes % 60
+    "#{hours}h #{rest}m"
   end
 end
--- spec/helpers/tickets_spec.rb
+++ spec/helpers/tickets_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe TicketsHelper, type: :helper do
   let(:user) { FactoryBot.create(:user) }
   let(:mhp) { FactoryBot.create(:category, name: "MHP") }
   let(:tms) { FactoryBot.create(:category, name: "TMS") }
-  let(:ticket_one) { FactoryBot.create(:ticket, category: mhp) }
-  let(:ticket_two) { FactoryBot.create(:ticket, category: tms) }
-  let(:ticket_three) { FactoryBot.create(:ticket, category: tms) }
+  let(:ticket_one) { FactoryBot.create(:ticket, category: mhp, duration: 32) }
+  let(:ticket_two) { FactoryBot.create(:ticket, category: tms, duration: 10) }
+  let(:ticket_three) { FactoryBot.create(:ticket, category: tms, duration: 65) }
   let(:modays_tickets) { [ ticket_one ] }
   let(:tuesdays_tickets) { [ ticket_two, ticket_three ] }
   let(:week_tickets) do
@@ -36,7 +36,7 @@ RSpec.describe TicketsHelper, type: :helper do
 
   describe "#total_tickets" do
     it "return hash of categories with count" do
-      expect(all_categories_with_count(week_tickets)).to eq( { MHP: 1, TMS: 2 } )
+      expect(all_categories_stats(week_tickets)).to eq( { MHP: { count: 1, duration: 32 }, TMS: { count: 2, duration: 75 } } )
     end
   end
 end
