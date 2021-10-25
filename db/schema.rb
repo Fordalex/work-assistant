203ad5fab0e5d527aeae00f1802d5738f4d1314b
Fordalex <alex96ford19@gmail.com>
2021-10-25 21:51:01 +0100

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_192423) do
+ActiveRecord::Schema.define(version: 2021_10_25_204144) do
 
   create_table "categories", force: :cascade do |t|
     t.integer "user_id"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2021_10_25_192423) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "member_groups", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "ticket_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["member_id"], name: "index_member_groups_on_member_id"
+    t.index ["ticket_id"], name: "index_member_groups_on_ticket_id"
   end
 
   create_table "members", force: :cascade do |t|
