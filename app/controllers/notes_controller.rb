c16a60a638ec1c680f4d8a38ef73fdebe6a610f7
Fordalex <alex96ford19@gmail.com>
2021-10-31 12:20:23 +0000

    adds filter form no logic

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,10 @@
-class NotesController < SessionsController
-    def notes
-      @tickets = Ticket.where(user: current_user)
-    end
+class NotesController < TicketsController
+  before_action :set_members, only: %i[notes]
+  before_action :set_subjects, only: %i[notes]
+  before_action :set_languages, only: %i[notes]
+  before_action :set_categories, only: %i[notes]
+
+  def notes
+    @tickets = Ticket.where(user: current_user)
+  end
 end
