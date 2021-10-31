d4ad54b0a1e3d94735df5cff18f5920e09635ce2
Fordalex <alex96ford19@gmail.com>
2021-10-31 23:51:48 +0000

    adds language form and controller

---
+++ app/controllers/languages_controller.rb
@@ -0,0 +1,30 @@
+class LanguagesController < SessionsController
+  before_action :set_language, only: %i[destroy]
+
+  def create
+    @language = Language.new(language_params.merge(user: current_user))
+    if @language.save
+      flash[:success] = "#{@language.name} has been saved!"
+    else
+      flash[:warning] = "An error occurred please try again."
+    end
+    redirect_to cms_path
+  end
+
+  def destroy
+    if @language.destroy
+      flash[:success] = "Language has been destroyed"
+    end
+    redirect_to cms_path
+  end
+
+  private
+
+  def language_params
+    params.require(:language).permit(:name, :colour)
+  end
+
+  def set_language
+    @language = Language.find(params[:id])
+  end
+end
