6c3ca94ae9bcf23b3a7fa74f8ab660a677e2df90
Fordalex <alex96ford19@gmail.com>
2021-11-01 11:53:32 +0000

    adds button and logic to destroy categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[destroy]
+
   def create
     category = Category.new(category_params.merge(user: current_user))
     if category.save
@@ -9,7 +11,18 @@ class CategoriesController < ApplicationController
     redirect_to cms_path
   end
 
+  def destroy
+    if @category.destroy
+      flash[:success] = "Category destroyed"
+    end
+    redirect_to cms_path
+  end
+
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:colour, :name)
