f91ce653a5f4109d6ed1786edb3e31900592147b
Fordalex <alex96ford19@gmail.com>
2021-11-06 21:40:35 +0000

    updates some styles

--- app/javascript/packs/set_theme.js
+++ app/javascript/packs/set_theme.js
@@ -3,9 +3,10 @@ window.addEventListener('load', function() {
   const themeColours = {
     'light' : {
       '--dark-bg-colour': '#fafcff',
-      '--mid-bg-colour': '#ffffff',
-      '--light-bg-colour': '#ffffff',
+      '--mid-bg-colour': '#fafcff',
+      '--light-bg-colour': '#eff2f6',
       '--input-colour': '#f9f9f9',
+      '--border-colour': '#f0f0f0',
       '--text-colour': 'rgb(0, 0, 0)',
       '--shadow-opacity': '0.1',
       '--glow-opacity': '0'
@@ -15,9 +16,10 @@ window.addEventListener('load', function() {
       '--mid-bg-colour': '#222222',
       '--light-bg-colour': '#3d3d3d',
       '--input-colour': '#000',
+      '--border-colour': '#424242',
       '--text-colour': 'rgb(221, 221, 221)',
       '--shadow-opacity': '0.8',
-      '--glow-opacity': '0.7'
+      '--glow-opacity': '0.3'
     }
   }
 
