8090893d91415ccec6101654eb11e1dbe8858c84
Fordalex <alex96ford19@gmail.com>
2021-11-06 11:43:27 +0000

    chagnes the css variables by the users theme

--- app/javascript/packs/set_theme.js
+++ app/javascript/packs/set_theme.js
@@ -1,8 +1,25 @@
-window.addEventListener("load", function() {
-  document.documentElement.style.setProperty('--dark-bg-colour', '#181818');
-  document.documentElement.style.setProperty('--mid-bg-colour', '#222222');
-  document.documentElement.style.setProperty('--light-bg-colour', '#3d3d3d');
-  document.documentElement.style.setProperty('--text-colour', 'rgb(221, 221, 221)');
-  document.documentElement.style.setProperty('--shadow-opacity', '0.8');
-  document.documentElement.style.setProperty('--glow-opacity', '0.7');
-})
+window.addEventListener('load', function() {
+  const theme = document.querySelector('[data-theme]').dataset.theme;
+  const themeColours = {
+    'light' : {
+      '--dark-bg-colour': '#fafcff',
+      '--mid-bg-colour': '#ffffff',
+      '--light-bg-colour': '#ffffff',
+      '--text-colour': 'rgb(0, 0, 0)',
+      '--shadow-opacity': '0.1',
+      '--glow-opacity': '0'
+    },
+    'dark' : {
+      '--dark-bg-colour': '#181818',
+      '--mid-bg-colour': '#222222',
+      '--light-bg-colour': '#3d3d3d',
+      '--text-colour': 'rgb(221, 221, 221)',
+      '--shadow-opacity': '0.8',
+      '--glow-opacity': '0.7'
+    }
+  }
+
+  Object.entries(themeColours[theme]).forEach(([k,v]) => {
+    document.documentElement.style.setProperty(k,v);
+  });
+});
