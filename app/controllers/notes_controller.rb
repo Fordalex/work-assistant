c10fe29fdae61d2b29ba718468fb6f9857f86981
Fordalex <alex96ford19@gmail.com>
2021-11-06 17:27:56 +0000

    adds the logic to filter tickets by checkboxes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,10 +4,79 @@ class NotesController < TicketsController
   before_action :set_languages, only: %i[notes]
   before_action :set_categories, only: %i[notes]
 
+  # TODO refactor this controller
+  # TODO Write some tests!!
+
   def notes
-    @tickets = Ticket.where(user: current_user)
+    @tickets = find_tickets
+
+    @searched_members = if params[:members].present?
+      params[:members][0].keys
+    else
+      []
+    end
+    @searched_subjects = if params[:subjects].present?
+      params[:subjects][0].keys
+    else
+      []
+    end
+    @searched_languages = if params[:languages].present?
+      params[:languages][0].keys
+    else
+      []
+    end
+  end
+
+  private
+
+  def find_tickets
+    tickets = Ticket.where(user: current_user)
+    filter_by_category(tickets)
+  end
+
+  def filter_by_category(tickets)
     if params[:categories].present?
-      @tickets = @tickets.joins(:category).where(categories: {id: params[:categories]})
+      tickets = tickets.joins(:category).where(categories: {id: params[:categories]})
+    end
+    filter_by_subject(tickets)
+  end
+
+  def filter_by_subject(tickets)
+    if params[:subjects].present?
+      tickets = tickets.filter do |ticket|
+        ticket_subjects = ticket.subject_groups.reduce([]) { |a,sg| a << sg.subject.name }
+        searched_subjects = params[:subjects][0].keys
+        searched_subjects.all? { |i| ticket_subjects.include?(i) }
+      end
+    else
+      tickets
+    end
+    filter_by_member(tickets)
+  end
+
+  def filter_by_member(tickets)
+    if params[:members].present?
+      tickets = tickets.filter do |ticket|
+        ticket_members = ticket.member_groups.reduce([]) { |a,sg| a << sg.member.name }
+        searched_members = params[:members][0].keys
+        searched_members.all? { |i| ticket_members.include?(i) }
+      end
+    else
+      tickets
+    end
+    filter_by_technology(tickets)
+  end
+
+  def filter_by_technology(tickets)
+    if params[:languages].present?
+      tickets = tickets.filter do |ticket|
+        ticket_languages = ticket.language_groups.reduce([]) { |a,sg| a << sg.language.name }
+        searched_languages = params[:languages][0].keys
+
+        searched_languages.all? { |i| ticket_languages.include?(i) }
+      end
+    else
+      tickets
     end
   end
 end
