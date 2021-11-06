4cacfd94efd95494f25d66af0c4c3646361b2eef
Fordalex <alex96ford19@gmail.com>
2021-11-06 11:42:46 +0000

    sets the setting or creates one if not present

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :set_setting
+
+  private
+
+  def set_setting
+    if user_signed_in?
+      @setting = Setting.find_by(user: current_user)
+      unless @setting
+        create_setting
+      end
+    end
+  end
+
+  def create_setting
+    Setting.create(user: current_user)
+    set_setting
+  end
 end
