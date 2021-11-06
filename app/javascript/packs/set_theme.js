6702b6bc9d08035c9035f5fe13f7fbab542578c9
Fordalex <alex96ford19@gmail.com>
2021-11-06 01:11:36 +0000

    sets colours to variables
    js will be able to toggle the theme from light to dark.

---
+++ app/javascript/packs/set_theme.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+  document.documentElement.style.setProperty('--dark-bg-colour', '#181818');
+  document.documentElement.style.setProperty('--mid-bg-colour', '#222222');
+  document.documentElement.style.setProperty('--light-bg-colour', '#3d3d3d');
+  document.documentElement.style.setProperty('--text-colour', 'rgb(221, 221, 221)');
+  document.documentElement.style.setProperty('--shadow-opacity', '0.8');
+  document.documentElement.style.setProperty('--glow-opacity', '0.7');
+})
