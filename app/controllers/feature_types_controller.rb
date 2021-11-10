1b56e677cd19e9ed8c988449da47d52e41e5ea30
Fordalex <alex96ford19@gmail.com>
2021-11-10 22:19:30 +0000

    adds the new and edit functionality for feature types

--- app/controllers/feature_types_controller.rb
+++ app/controllers/feature_types_controller.rb
@@ -1,4 +1,9 @@
 class FeatureTypesController < ApplicationController
+  before_action :set_featue_type, only: %i[edit update]
+
+  def new
+  end
+
   def create
     @feature_type = FeatureType.new(feature_types_params.merge(user: current_user))
     if @feature_type.save!
@@ -9,7 +14,23 @@ class FeatureTypesController < ApplicationController
     redirect_to cms_path
   end
 
+  def edit
+  end
+
+  def update
+    if @feature_type.update(feature_types_params)
+
+    else
+
+    end
+    redirect_to cms_path
+  end
+
   private
+
+  def set_featue_type
+    @feature_type = FeatureType.find(params[:id])
+  end
 
   def feature_types_params
     params.require(:feature_type).permit(:name, :content, :colour)
