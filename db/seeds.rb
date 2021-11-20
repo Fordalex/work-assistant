73ad06ae17e6904bce2f3a0f43cf1721fa17339a
Fordalex <alex96ford19@gmail.com>
2021-11-20 13:51:18 +0000

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,8 @@ FeatureGroup.destroy_all
 FeatureType.destroy_all
 Ticket.destroy_all
 User.destroy_all
+UserGroup.destroy_all
+Collection.destroy_all
 
 puts "\n== Creating users =="
 
@@ -26,17 +28,64 @@ second_user = User.create!(
 
 puts "\n== Creating collections =="
 
-personal_collection = Collection.create!()
-group_collection = Collection.create!()
+personal_collection = Collection.create!(
+  name: "Personal"
+)
+group_collection = Collection.create!(
+  name: "Group"
+)
+
+puts "\n== Creating user groups =="
+
+UserGroup.create(
+  collection: personal_collection,
+  user: first_user
+)
+
+UserGroup.create(
+  collection: group_collection,
+  user: first_user
+)
+
+UserGroup.create(
+  collection: group_collection,
+  user: second_user
+)
 
 puts "\n== Creating feature types =="
 
+subject = FeatureType.create!(
+  collection: personal_collection,
+  name: "Subjects"
+)
+
 puts "\n== Creating features =="
+
+Feature.create!(
+  name: "Arkiane",
+  content: "Not sure about his attribute??",
+  feature_type: subject,
+)
+
+Feature.create!(
+  name: "Elite",
+  content: "Not sure about his attribute??",
+  feature_type: subject,
+)
 
 puts "\n== Creating tickets =="
 
 ticket_one = Ticket.create(
   collection: personal_collection,
+  date: Date.today,
+  title: "MHP",
+  description: "Fix layout issue with the parks navigation, Tony has request this to be changed.",
+  duration: 60,
+  start_time: Time.now + 1.hour
+)
+
+ticket_one = Ticket.create(
+  collection: group_collection,
   date: Date.today,
   title: "MHP",
   description: "Fix layout issue with the parks navigation, Tony has request this to be changed.",
