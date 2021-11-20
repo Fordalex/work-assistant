58d28e9252b1451d4197e7396fba20c1d95d7973
Fordalex <alex96ford19@gmail.com>
2021-11-20 15:25:11 +0000

    adds form and logic to create new collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -6,6 +6,20 @@ class CollectionsController < ApplicationController
     @collections = Collection.joins(:user_groups).where(user_groups:{ user: current_user})
   end
 
+  def new
+    @collection = Collection.new
+  end
+
+  def create
+    @collection = Collection.new(collection_params)
+    if @collection.save!
+      UserGroup.create!(user: current_user, collection: @collection)
+      redirect_to collections_path
+    else
+      render 'new'
+    end
+  end
+
   def set_collection
     if Collection.find_by(id: params[:id])
       session[:collection_id] = params[:id]
@@ -13,5 +27,11 @@ class CollectionsController < ApplicationController
     else
       redirect_to collections_path
     end
+  end
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:name)
   end
 end
