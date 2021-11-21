bfb2597e3952b81d3a7c1c60884dc665e5f86a59
Fordalex <alex96ford19@gmail.com>
2021-11-21 16:46:47 +0000

    updates spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 RSpec.describe Ticket, type: :model do
   describe "Association" do
     it { should belong_to(:collection) }
+    it { should have_many(:feature_groups) }
   end
 end
