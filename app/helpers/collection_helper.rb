354011a9a571679730173673681044594de16710
Fordalex <alex96ford19@gmail.com>
2021-11-21 16:43:15 +0000

    updates collection template

--- app/helpers/collection_helper.rb
+++ app/helpers/collection_helper.rb
@@ -6,27 +6,33 @@ module CollectionHelper
     )
     Feature.create!(
       name: "Code",
-      feature_type: technical
+      feature_type: technical,
+      feature_ticket_type: "Text"
     )
     Feature.create!(
       name: "Resource",
-      feature_type: technical
+      feature_type: technical,
+      feature_ticket_type: "Text"
     )
     language = FeatureType.create!(
       collection: collection,
-      name: "Languages"
+      name: "Languages",
+      feature_ticket_type: "Select"
     )
     Feature.create!(
       name: "HTML",
-      feature_type: language
+      feature_type: language,
+      feature_ticket_type: "Select"
     )
     Feature.create!(
       name: "CSS",
-      feature_type: language
+      feature_type: language,
+      feature_ticket_type: "Select"
     )
     Feature.create!(
       name: "JS",
-      feature_type: language
+      feature_type: language,
+      feature_ticket_type: "Select"
     )
   end
 end
